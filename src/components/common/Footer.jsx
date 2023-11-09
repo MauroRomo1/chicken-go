3030483b803fe737740ef7a48f747bd475053649
dharez <mauroromo.developer@outlook.com>
2023-11-09 01:35:18 -0300

    Terminando el footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -8,20 +8,18 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white">
+    <footer className="bg-dark">
       <div className="container p-3">
         <div className="row">
           <div className="col-12 col-lg-4 mb-4">
             <img
               src="/logo.png"
               alt="Logo de un pollo corriendo"
-              className="mx-auto d-block"
-              width="150"
-              height="150"
+              className="mx-auto d-block logoFooter"
             />
           </div>
           <div className="col-12 col-lg-4 mb-4 text-center">
-            <h5>Mas opciones</h5>
+            <h5 className="text-white">Mas opciones</h5>
             <ul className="nav flex-column">
               <li className="nav-item">
                 <Link className="nav-link moreOptions" to="*">
@@ -41,13 +39,13 @@ const Footer = () => {
             </ul>
           </div>
           <div className="col-12 col-lg-4 mb-4 text-center">
-            <h5>Redes sociales</h5>
+            <h5 className="text-white">Redes sociales</h5>
             <ul className="nav justify-content-center">
               <li className="nav-item">
                 <a
                   href="https://www.facebook.com/"
                   target="_blank"
-                  className="nav-link text-primary"
+                  className="nav-link facebookSocial"
                 >
                   <FontAwesomeIcon icon={faFacebook} size="xl" />
                 </a>
@@ -56,7 +54,7 @@ const Footer = () => {
                 <a
                   href="https://twitter.com/"
                   target="_blank"
-                  className="nav-link text-info"
+                  className="nav-link twitterSocial"
                 >
                   <FontAwesomeIcon icon={faTwitter} size="xl" />
                 </a>
@@ -65,7 +63,7 @@ const Footer = () => {
                 <a
                   href="https://www.instagram.com/"
                   target="_blank"
-                  className="nav-link text-danger"
+                  className="nav-link instagramSocial"
                 >
                   <FontAwesomeIcon icon={faInstagram} size="xl" />
                 </a>
@@ -73,6 +71,11 @@ const Footer = () => {
             </ul>
           </div>
         </div>
+      </div>
+      <div className="bg-copyright">
+        <h6 className="text-center text-white mb-0 p-2">
+          &copy; 2023 Todos los derechos reservados
+        </h6>
       </div>
     </footer>
   );
