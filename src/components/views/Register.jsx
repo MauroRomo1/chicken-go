8f6bf89e6c07736f5e8f96c52834a02ea1b495e2
dharez <mauroromo.developer@outlook.com>
2023-11-13 23:00:36 -0300

    Empezando con las validaciones para el registro de usuarios

--- src/components/views/Register.jsx
+++ src/components/views/Register.jsx
@@ -7,11 +7,24 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
   useEffect(() => {
     document.title = "Chicken go | Registro";
   }, []);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm();
+
+  const onSubmit = (user) => {
+    console.log(user);
+  };
 
   return (
     <div className="container my-5">
@@ -27,26 +40,50 @@ const Register = () => {
                 />
               </div>
               <h6 className="text-center my-3">
-              REGÍSTRATE Y DISFRUTA DE LOS MEJORES BENEFICIOS
+                REGÍSTRATE Y DISFRUTA DE LOS MEJORES BENEFICIOS
               </h6>
             </div>
             <div className="col-12 col-lg-6 border">
               <h6 className="text-center my-3">Formulario para registrarse</h6>
-              <Form className="formRegister" noValidate>
+              <Form
+                className="formRegister"
+                noValidate
+                onSubmit={handleSubmit(onSubmit)}
+              >
                 <Form.Group className="mb-3" controlId="name">
                   <Form.Label>
                     <FontAwesomeIcon icon={faCircleUser} className="me-2" />
                     Nombre
                   </Form.Label>
-                  <Form.Control type="text" placeholder="Ingrese su nombre" />
+                  <Form.Control
+                    type="text"
+                    isInvalid={errors.name}
+                    placeholder="Ingrese su nombre"
+                    {...register("name", {
+                      required: "El nombre es un dato obligatorio",
+                    })}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.name?.message}
+                  </Form.Control.Feedback>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="lastName">
+                <Form.Group className="mb-3" controlId="lastname">
                   <Form.Label>
                     <FontAwesomeIcon icon={faCircleUser} className="me-2" />
                     Apellido
                   </Form.Label>
-                  <Form.Control type="text" placeholder="Ingrese su apelido" />
+                  <Form.Control
+                    type="text"
+                    isInvalid={errors.lastname}
+                    placeholder="Ingrese su apelido"
+                    {...register("lastname", {
+                      required: "El apellido es un dato obligatorio",
+                    })}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.lastname?.message}
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="email">
@@ -54,7 +91,17 @@ const Register = () => {
                     <FontAwesomeIcon icon={faEnvelope} className="me-2" />
                     Correo electronico
                   </Form.Label>
-                  <Form.Control type="email" placeholder="Ingrese su email" />
+                  <Form.Control
+                    type="email"
+                    isInvalid={errors.email}
+                    placeholder="Ingrese su email"
+                    {...register("email", {
+                      required: "El correo electronico es un dato obligatorio",
+                    })}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.email?.message}
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="password">
@@ -64,8 +111,15 @@ const Register = () => {
                   </Form.Label>
                   <Form.Control
                     type="password"
+                    isInvalid={errors.password}
                     placeholder="Ingrese una contraseña"
+                    {...register("password", {
+                      required: "La contraseña es un dato obligatorio",
+                    })}
                   />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.password?.message}
+                  </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="passwordConfirm">
                   <Form.Label>
@@ -74,15 +128,29 @@ const Register = () => {
                   </Form.Label>
                   <Form.Control
                     type="password"
+                    isInvalid={errors.passwordConfirm}
                     placeholder="Repita la contraseña"
+                    {...register("passwordConfirm", {
+                      required: "La repeticion de la contraseña es obligatoria",
+                    })}
                   />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.passwordConfirm?.message}
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="checkTerms">
                   <Form.Check
                     type="checkbox"
+                    isInvalid={errors.checkTerms}
                     label="Aceptó los términos y condiciones"
+                    {...register("checkTerms", {
+                      required: "Debes de aceptar los terminos y condiciones",
+                    })}
                   />
+                  <Form.Text className="text-danger" type="invalid">
+                    {errors.checkTerms?.message}
+                  </Form.Text>
                 </Form.Group>
                 <div className="d-grid mx-auto col col-md-8 my-3">
                   <Button className="btnSubmit" type="submit">
